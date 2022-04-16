88de153dbed3b04ad990b8a0b7c6b95a4006e0c6
kth5954 <kth5954@gmail.com>
2022-04-16 17:23:30 +0900

    saturday

--- src/index.js
+++ src/index.js
@@ -10,26 +10,13 @@ toggle.addEventListener("click", (event)=>{
 })
 
 
-// image slider
-// var slider = document.querySelector("#slider");
-// var slides = slider.querySelector(".slides");
-// var slide = slides.querySelectorAll(".slide");
 
-// var currentSlide = 0;
+//Typeit Text animation
+document.addEventListener("DOMContentLoaded", ()=>{
+    new TypeIt(".welcome")
+    .pause(500)
+    .delete(100, {delay: 50})
+    .type("천천히 둘러보세요!! 😎:) <br> Take it slow!! 😎:)")
 
-// setInterval(function() {
-//     var from = -(1024 * currentSlide);
-//     var to = from - 1024;
-//     slides.animate({
-//         marginLeft: [from + "px", to + "px"]
-//     }, {
-//         duration: 500,
-//         easing: "ease",
-//         iterations: 1,
-//         fill: "both"
-//     });
-//     currentSlide++;
-//     if (currentSlide === (slide.length - 1)) {
-//         currentSlide = 0;
-//     }
-// }, 3000);
+    .go();
+});
